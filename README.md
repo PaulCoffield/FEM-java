ee973b0f31366aa1766c7d3d965a8dde1f742ca9
Paul Coffield <paulcoffield@Pauls-MacBook-Pro.local>
2023-09-06 20:23:19 +0100

    Basics app, intro

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FEM-java
 Frontend Masters Java Course
+
+[https://frontendmasters.com/courses/java](https://frontendmasters.com/courses/java)
---
+++ java-course/src/main/java/decision_structure/QuotaCalculator.java
@@ -0,0 +1,29 @@
+package decision_structure;
+
+import java.util.Scanner;
+public class QuotaCalculator {
+
+    public static void main(String[] args) {
+
+        int quota = 10;
+
+        System.out.println("Enter the number of sales made");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+
+        if()
+
+        /* My attempt before watching the video
+
+        if(sales > quota){
+            System.out.println("Excellent work on hitting over your quota!");
+        }else if(sales < quota){
+            int deficit = quota - sales;
+            System.out.println("Uh oh, you were short by " + deficit + " sales this week.");
+        }
+         */
+
+        scanner.close();
+
+    }
+}
